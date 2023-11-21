d93e8f6f40b3af2dd2df1907a402ff35a5b090cb
Igors Volohovs <igorsvolohovs@gmail.com>
2023-11-21 22:28:08 +0200

    Update main.cxx

--- TaskTools/main.cxx
+++ TaskTools/main.cxx
@@ -1,16 +1,16 @@
-#include <iostream>
-
 import io;
 import config.config;
 import test.test;
 import test.benchmark;
 
 int main(int argc, char** argv) {
+    // Configure program
+    config::parser();
+
     // Run Google Test
     test::run(argc, argv);
 
     // Run Google Benchmark
     benchmark::run(argc, argv);
-
-    return EXIT_SUCCESS;
 }
+
