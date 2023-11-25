297af8e7629a364b39ae11d5ebea3e4f59649bc3
Igors Volohovs <igorsvolohovs@gmail.com>
2023-11-25 02:08:30 +0200

    Update and rename run.bat to run.ps1

---
+++ TaskTools/run.ps1
@@ -0,0 +1,23 @@
+# Удаляем каталог build, если он существует
+if (Test-Path -Path "build") {
+    Remove-Item -Recurse -Force "build"
+}
+
+# Устанавливаем политику выполнения для запуска скриптов
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+# Выполняем установку зависимостей через Conan
+conan install . --build=missing --output-folder build -c tools.env.virtualenv:powershell=True 
+
+# Запускаем CMake с заданным пресетом
+cmake --preset conan-default
+
+# Выполняем скрипты сборки и запуска, сгенерированные Conan
+& ".\build\build\generators\conanbuild.ps1"
+& ".\build\build\generators\conanrun.ps1"
+
+# Собираем проект с использованием CMake
+cmake --build --preset conan-release
+
+# Запускаем исполняемый файл
+& ".\build\build\Release\main.exe"
