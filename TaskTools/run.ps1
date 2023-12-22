dc7ccae1d16c2dd9f24bcf0444305ad6f930d008
Igors Volohovs <igorsvolohovs@gmail.com>
2023-12-22 15:33:18 +0200

    Update run.ps1

--- TaskTools/run.ps1
+++ TaskTools/run.ps1
@@ -10,6 +10,10 @@ if (Test-Path -Path "build") {
     Remove-Item -Recurse -Force "build"
 }
 
+if (Test-Path -Path "cmake") {
+    Remove-Item -Recurse -Force "cmake"
+}
+
 # Install dependencies through Conan
 $ErrorActionPreference = 'Stop'
 conan install . --deployer=full_deploy --build=missing --output-folder build -c tools.env.virtualenv:powershell=True
@@ -19,16 +23,24 @@ if ($runMode -eq 1)
     # Run with 'conan-release' preset
     cmake --preset conan-release
     Write-Output "CMake has been successfully executed with preset conan-release!"
-    & ".\build\build\Release\generators\conanbuild.ps1"
-    & ".\build\build\Release\generators\conanrun.ps1"
+    & ".\build\cmake\conanbuild.ps1"
+    & ".\build\cmake\conanrun.ps1"
     cmake --build . --preset conan-release
     .\build\build\Release\task.exe
 } elseif ($runMode -eq 2) {
     # Run with 'conan-default' preset
     cmake --preset conan-default
     Write-Output "CMake has been successfully executed with preset conan-default!"
-    & ".\build\build\generators\conanbuild.ps1"
-    & ".\build\build\generators\conanrun.ps1"
+    & ".\build\cmake\conanbuild.ps1"
+    & ".\build\cmake\conanrun.ps1"
     cmake --build . --preset conan-release
     .\build\build\Release\task.exe    
 }
+
+# Экспорт пакета с помощью Conan
+conan export-pkg . --name=task --version=1.0 --user=user --channel=testing
+
+# Запуск ISCC.exe с аргументами
+& 'C:\Program Files (x86)\Inno Setup 6\ISCC.exe' .\build\task-1.0.iss
+
+
