96782bf9b3d5e1b682d2cc70093f25c9b6cebc36
Igors Volohovs <igorsvolohovs@gmail.com>
2023-12-22 20:56:43 +0200

    Update run.ps1

--- TaskTools/run.ps1
+++ TaskTools/run.ps1
@@ -26,21 +26,32 @@ if ($runMode -eq 1)
     & ".\build\cmake\conanbuild.ps1"
     & ".\build\cmake\conanrun.ps1"
     cmake --build . --preset conan-release
-    .\build\build\Release\task.exe
 } elseif ($runMode -eq 2) {
     # Run with 'conan-default' preset
     cmake --preset conan-default
     Write-Output "CMake has been successfully executed with preset conan-default!"
     & ".\build\cmake\conanbuild.ps1"
     & ".\build\cmake\conanrun.ps1"
-    cmake --build . --preset conan-release
-    .\build\build\Release\task.exe    
+    cmake --build . --preset conan-release   
+}
+
+# поиск task.exe в директории build
+$taskExe = Get-ChildItem -Path .\build -Filter task.exe -Recurse -ErrorAction SilentlyContinue
+
+# запуск task.exe
+if ($taskExe) {
+    Write-Output "task.exe has been found!"
+    & $taskExe.FullName
+} else {
+    Write-Output "task.exe has not been found!"
+    exit 1
 }
 
 # Экспорт пакета с помощью Conan
 conan export-pkg . --name=task --version=1.0 --user=user --channel=testing
 
-# Запуск ISCC.exe с аргументами
-& 'C:\Program Files (x86)\Inno Setup 6\ISCC.exe' .\build\task-1.0.iss
+# Запуск ISCC.exe с полным путем к файлу .\pkg-setup\task-1.0.iss + перемести task-1.0-setup.exe в директорию pkg-setup
+& "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" .\pkg-setup\task-1.0.iss
+& Move-Item -Path .\task-1.0-setup.exe -Destination .\pkg-setup -Force
 
 
