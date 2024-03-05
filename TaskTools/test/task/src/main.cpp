b028f4072a747dbfc19875800cbbac1e9dfa3647
Igors Volohovs <igorsvolohovs@gmail.com>
2024-03-05 21:54:23 +0200

    Update main.cpp

--- TaskTools/test/task/src/main.cpp
+++ TaskTools/test/task/src/main.cpp
@@ -1,13 +1,25 @@
 #include "test.hpp"
 #include "benchmark.hpp"
+#include "stopwatch.hpp"
 
 int main(int argc, char** argv) {
+    stopwatch::Stopwatch w;
+    
+
     // Configure program
+    w.start();
     config::parser();
+    std::cout << "Elapsed: " << w.lap<stopwatch::ns>() << " milliseconds." << std::endl;
 
     // Run Google Test
+    w.start();
     test::run();
+    std::cout << "Elapsed: " << w.lap<stopwatch::ns>() << " milliseconds." << std::endl;
 
     // Run Google Benchmark
+    w.start();
     benchmark::run();
+    std::cout << "Elapsed: " << w.lap<stopwatch::ns>() << " milliseconds." << std::endl;
+
+    return 0;
 }
