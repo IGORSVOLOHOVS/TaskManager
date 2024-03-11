c2831963818289f30296f0cf095dabaf63262906
Igors Volohovs <igorsvolohovs@gmail.com>
2024-03-11 10:07:03 +0200

    create to do list and remove somethink....

--- TaskTools/_help/cpp_python/include/custom_include.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <benchmark.hpp>
-#include <config.hpp>
-#include <test.hpp>
-#include <io.hpp>
--- TaskTools/_help/cpp_python/task.py
+++
@@ -1,8 +0,0 @@
-import pytask as t
-import sys
-
-t.parser("config.yaml")
-
-t.run_benchmark(sys.argv[1:])
-
-t.run_test(sys.argv[1:])
