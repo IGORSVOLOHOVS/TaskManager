bc1dd2cf540934ccae427f07aa61686fd2feeb3c
Igors Volohovs <igorsvolohovs@gmail.com>
2024-05-05 02:15:17 +0300

    Update connector.hpp

--- TaskTools/include/connector.hpp
+++ TaskTools/include/connector.hpp
@@ -6,11 +6,18 @@
 #include <cstring>
 #include <unistd.h>
 
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
 #include <chrono>
 #include <thread>
 
 #include "log.hpp"
 #include "parralel.hpp"
+#include "io.hpp"
 
 constexpr size_t BUFFER_SIZE = 8192;
 
@@ -21,22 +28,21 @@ struct ConnectorData
     Log log;
 };
 
+enum class Command : unsigned int
+{
+    SIMPLE = 0,
+    VERSION,
+    HELP
+};
+
+struct ParsedResponse
+{
+    Command command;
+    char *data;
+};
+
 class Connector
 {
-public:
-    enum class Command : unsigned int
-    {
-        SIMPLE = 0,
-        VERSION,
-        HELP
-    };
-
-    struct ParsedResponse
-    {
-        Command command;
-        char *data;
-    };
-
 private:
     void send(const char *data)
     {
@@ -159,7 +165,7 @@ public:
             {
                 break;
             }
-            speak(Connector::Command::SIMPLE, message.c_str());
+            speak(Command::SIMPLE, message.c_str());
         }
     }
 
@@ -167,5 +173,205 @@ public:
         AsyncFunction(speak, void) private : mqd_t sender;
     mqd_t receiver;
 
+    ConnectorData data;
+};
+
+struct Network
+{
+    std::string ip;
+    int port;
+};
+
+class ServerTCP
+{
+public:
+    ServerTCP(Network network, ConnectorData data = {0, nullptr}) : data(data), network(network)
+    {
+        // Creating socket file descriptor
+        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        {
+            perror("socket failed");
+            exit(EXIT_FAILURE);
+        }
+
+        // Forcefully attaching socket to the port 8080
+        if (setsockopt(server_fd, SOL_SOCKET,
+                       SO_REUSEADDR | SO_REUSEPORT, &opt,
+                       sizeof(opt)))
+        {
+            perror("setsockopt");
+            exit(EXIT_FAILURE);
+        }
+        address.sin_family = AF_INET;
+        address.sin_port = htons(network.port);
+        address.sin_addr.s_addr = inet_addr(network.ip.c_str());
+
+        // Forcefully attaching socket to the port 8080
+        if (bind(server_fd, (struct sockaddr *)&address,
+                 sizeof(address)) < 0)
+        {
+            perror("bind failed");
+            exit(EXIT_FAILURE);
+        }
+        if (::listen(server_fd, 3) < 0)
+        {
+            perror("listen");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    ~ServerTCP()
+    {
+        close(server_fd);
+    }
+
+    void listen(size_t period_ms = 1000)
+    {
+        int new_socket;
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+        {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+        while(true) {
+            if((valread = read(new_socket, buffer, 1024 - 1)) == 0) {
+                throw std::runtime_error("Failed to read message");
+            }
+
+            buffer[valread] = '\0';
+
+            ParsedResponse response = parse(buffer);
+
+            char *result = execute(response);
+            if (result)
+            {
+                if(send(new_socket, result, strlen(result), 0) == -1) {
+                    throw std::runtime_error("Failed to send message");
+                }
+            }
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(period_ms));
+        }    
+        close(new_socket);
+    }
+
+private:
+    ParsedResponse parse(char *response)
+    {
+        ParsedResponse parsed_response;
+        parsed_response.command = static_cast<Command>(response[0] - '0');
+        parsed_response.data = response + 1;
+        return parsed_response;
+    }
+
+    char *execute(ParsedResponse response)
+    {
+        switch (response.command)
+        {
+        case Command::SIMPLE:
+        {
+            std::cout << response.data << '\n';
+            return "OK";
+        }
+        case Command::VERSION:
+        {
+            std::cout << "Version 1.0.0\n";
+            return "OK";
+        }
+        case Command::HELP:
+        {
+            std::cout << "Usage: ./main [OPTION]...\n";
+            std::cout << "1 Options:\n";
+            std::cout << "2   -h, --help\t\t\tDisplay this information\n";
+            std::cout << "3   -v, --version\t\t\tDisplay the version\n";
+            return "OK";
+        }
+        default:
+            std::cerr << "Unknown command: " << static_cast<unsigned int>(response.command) << '\n';
+            return nullptr;
+        }
+
+        return nullptr;
+    }
+
+    int server_fd;
+    long valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+
+    Network network;
+    ConnectorData data;
+};
+
+class ClientTCP
+{
+public:
+    ClientTCP(Network network, ConnectorData data = {0, nullptr}) : data(data), network(network)
+    {
+        if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+            printf("\n Socket creation error \n");
+            throw std::runtime_error("Failed to create socket");
+        }
+    
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_port = htons(network.port);
+    
+        // Convert IPv4 and IPv6 addresses from text to binary
+        // form
+        if (inet_pton(AF_INET, network.ip.c_str(), &serv_addr.sin_addr) <= 0) {
+            throw std::runtime_error("Invalid address/ Address not supported");
+        }
+    
+        if ((status = connect(client_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0) {
+            throw std::runtime_error("Connection Failed");
+        }
+    }
+
+    ~ClientTCP()
+    {
+        close(client_fd);
+    }
+
+    std::string speak(Command command, const char *data = "")
+    {
+        char *request = new char[strlen(data) + 2];
+        request[0] = static_cast<unsigned int>(command) + '0';
+        strcpy(request + 1, data);
+        
+        if(send(client_fd, request, strlen(request), 0) == -1) {
+            throw std::runtime_error("Failed to send message");
+        }
+
+        if((valread = read(client_fd, buffer, 1024 - 1) == 0)) {
+            throw std::runtime_error("Failed to read message");
+        }  
+
+        return buffer;
+    }
+
+    std::string speak()
+    {
+        std::string message;
+        while (true)
+        {
+            message = read("Enter a word: ");
+            if (message == "!q")
+            {
+                break;
+            }
+            std::cout << speak(Command::SIMPLE, message.c_str()) << '\n';
+        }
+    }
+
+private:
+    int client_fd;
+    long valread;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+    int status;
+
+    Network network;
     ConnectorData data;
 };
