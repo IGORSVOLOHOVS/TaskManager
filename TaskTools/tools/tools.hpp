895ee93d7585546ec3021eddf4ee7aaaec81e67a
Igors Volohovs <igorsvolohovs@gmail.com>
2024-10-31 23:27:39 +0200

    Update tools.hpp

--- TaskTools/tools/tools.hpp
+++ TaskTools/tools/tools.hpp
@@ -20,6 +20,28 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 
+#include <atomic>
+#include <concepts>
+#include <deque>
+#include <functional>
+#include <future>
+#include <memory>
+#include <semaphore>
+#include <thread>
+#include <type_traits>
+#include <algorithm>
+#include <concepts>
+#include <deque>
+#include <mutex>
+#include <optional>
+
+#ifdef __has_include
+#    if __has_include(<version>)
+#        include <version>
+#    endif
+#endif
+
 #include "time.hpp"
 #include "logger.hpp"
 #include "parser.hpp"
+#include "thread_pool.h"
